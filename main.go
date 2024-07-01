d94ff2cbb6baf633a16c6ab512b2f7bbfa9da22c
horry <horry@pop-os.localdomain>
2024-07-01 12:17:12 +0200

    made simple config

--- main.go
+++ main.go
@@ -7,9 +7,44 @@ package main
 
 import (
 	"brige/app/server"
+	"errors"
+	"log"
+	"os"
+	"strconv"
 )
 
+type Config struct {
+	ServerConfig server.ServerConfig
+}
+
+func print_help() {
+	println(os.Args[0] + " <HOST> <PORT>")
+}
+
+func parse_args() (Config, error) {
+	if len(os.Args) != 3 {
+		print_help()
+		return Config{}, errors.New("incorect amount of arguments")
+	}
+	host := os.Args[1]
+	port, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		return Config{}, err
+	}
+
+	var config Config = Config{
+		ServerConfig: server.ServerConfig{
+			Host: host,
+			Port: port,
+		},
+	}
+	return config, nil
+}
+
 func main() {
-	server.Entry(server.ServerConfig{Host: "localhost", Port: 8000})
-	println("Hello World!")
+	config, err := parse_args()
+	if err != nil {
+		log.Fatal(err)
+	}
+	server.Entry(config.ServerConfig)
 }
